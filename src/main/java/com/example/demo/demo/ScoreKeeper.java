7096e5e7ebc65ea437914734b3ab47d3e67e17c3
rashifar <rashifar@students.zhaw.ch>
2023-06-03 11:26:35 +0200

    Java File erstellt

---
+++ src/main/java/com/example/demo/demo/ScoreKeeper.java
@@ -0,0 +1,13 @@
+package com.example.demo.demo;
+
+public class ScoreKeeper {
+    private int scoreTeam1;
+    private int scoreTeam2;
+
+    public ScoreKeeper() {
+        scoreTeam1 = 0;
+        scoreTeam2 = 0;
+
+    };
+
+}
