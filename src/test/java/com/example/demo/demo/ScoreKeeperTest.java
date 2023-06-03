9f531f2da17abb28fc2569ccdee1b9cd90840574
rashifar <rashifar@students.zhaw.ch>
2023-06-03 11:36:13 +0200

    Testing Team 1

--- src/test/java/com/example/demo/demo/ScoreKeeperTest.java
+++ src/test/java/com/example/demo/demo/ScoreKeeperTest.java
@@ -2,11 +2,20 @@ package com.example.demo.demo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ScoreKeeperTest {
 
     private ScoreKeeper scoreKeeper;
+
+    @BeforeEach
+
+    public void setUp() {
+
+        scoreKeeper = new ScoreKeeper();
+
+    }
 
     @Test
 
@@ -18,4 +27,33 @@ public class ScoreKeeperTest {
 
     }
 
+    @Test
+
+    public void testScoreTeam11() {
+
+        scoreKeeper.scoreTeam11();
+
+        assertEquals("001:000", scoreKeeper.getScore());
+
+    }
+
+    @Test
+
+    public void testScoreTeam12() {
+
+        scoreKeeper.scoreTeam12();
+
+        assertEquals("002:000", scoreKeeper.getScore());
+
+    }
+
+    @Test
+
+    public void testScoreTeam13() {
+
+        scoreKeeper.scoreTeam13();
+
+        assertEquals("003:000", scoreKeeper.getScore());
+
+    }
 }
