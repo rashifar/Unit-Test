4ede26398ab19cae42becc27673131d6828a8e1b
rashifar <rashifar@students.zhaw.ch>
2023-06-03 12:09:22 +0200

    Last step

--- src/test/java/com/example/demo/demo/ScoreKeeperTest.java
+++ src/test/java/com/example/demo/demo/ScoreKeeperTest.java
@@ -16,6 +16,7 @@ public class ScoreKeeperTest {
         scoreKeeper = new ScoreKeeper();
 
     }
+
     @Test
     public void testInitalScore() {
 
@@ -23,32 +24,38 @@ public class ScoreKeeperTest {
 
         assertEquals("000:000", scoreKeeper.getScore());
     }
+
     @Test
     public void testScoreTeam11() {
         scoreKeeper.scoreTeam11();
         assertEquals("001:000", scoreKeeper.getScore());
     }
+
     @Test
     public void testScoreTeam12() {
         scoreKeeper.scoreTeam12();
         assertEquals("002:000", scoreKeeper.getScore());
     }
+
     @Test
     public void testScoreTeam13() {
         scoreKeeper.scoreTeam13();
         assertEquals("003:000", scoreKeeper.getScore());
 
     }
+
     @Test
     public void testScoreTeam21() {
         scoreKeeper.scoreTeam21();
         assertEquals("001:000", scoreKeeper.getScore());
     }
+
     @Test
     public void testScoreTeam22() {
         scoreKeeper.scoreTeam22();
         assertEquals("002:000", scoreKeeper.getScore());
     }
+
     @Test
     public void testScoreTeam23() {
         scoreKeeper.scoreTeam23();
@@ -56,5 +63,38 @@ public class ScoreKeeperTest {
 
     }
 
+    @Test
+
+    public void testMultipleScoresTeam1() {
+
+        scoreKeeper.scoreTeam11();
+        scoreKeeper.scoreTeam12();
+        scoreKeeper.scoreTeam13();
+        assertEquals("006:000", scoreKeeper.getScore());
+    }
+
+    @Test
+
+    public void testMultipleScoresTeam2() {
+        scoreKeeper.scoreTeam21();
+        scoreKeeper.scoreTeam22();
+        scoreKeeper.scoreTeam23();
+        assertEquals("000:006", scoreKeeper.getScore());
+
+    }
+
+    @Test
+
+    public void testMultipleScoresBothTeams() {
+
+        scoreKeeper.scoreTeam11();
+        scoreKeeper.scoreTeam12();
+        scoreKeeper.scoreTeam13();
+        scoreKeeper.scoreTeam21();
+        scoreKeeper.scoreTeam23();
+
+        assertEquals("006:004", scoreKeeper.getScore());
+
+    }
 
 }
